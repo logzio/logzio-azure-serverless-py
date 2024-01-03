413fe3d9bbfbb24e1dcabb009b2b60a76f8b7761
bardab <bardabun@gmail.com>
2024-01-03 16:49:07 +0200

    update that work with 6 resources

--- LogzioShipper/__init__.py
+++ LogzioShipper/__init__.py
@@ -1,4 +1,6 @@
 import os
+import threading
+
 import azure.functions as func
 import logging
 import json
@@ -11,6 +13,8 @@ from threading import Thread
 from queue import Queue
 import backoff
 from typing import List
+import time
+from queue import Empty
 
 # Load environment variables
 load_dotenv()
@@ -37,14 +41,18 @@ HEADERS = {"Content-Type": "application/json"}
 RETRY_WAIT_FIXED = 2  # seconds for retry delay
 thread_count = int(os.getenv('THREAD_COUNT', 4))
 
-# Queue for logs
-log_queue = Queue()
+# Queue for batched logs
+batch_queue = Queue()
 
 # Backup Container
 backup_container = BackupContainer(logging, container_client)
 
 # Connection Pool (Session)
 session = Session()
+
+# Constants for batching
+BUFFER_SIZE = int(os.getenv('BUFFER_SIZE', 100))  # Batch size
+INTERVAL_TIME = int(os.getenv('INTERVAL_TIME', 5000)) / 1000  # Interval time in seconds
 
 
 def add_timestamp(log):
@@ -63,44 +71,79 @@ def delete_empty_fields_of_log(log):
 
 
 @backoff.on_exception(backoff.expo, requests.exceptions.RequestException, max_tries=3)
-def send_log(log):
-    response = session.post(LOGZIO_URL, params={"token": LOGZIO_TOKEN, "type": "type_log"}, headers=HEADERS, json=log)
-    response.raise_for_status()
+def send_batch(batch_data):
+    try:
+        # Join batch data to a single string before sending
+        batch_str = ''.join(batch_data)
+        response = session.post(LOGZIO_URL, params={"token": LOGZIO_TOKEN, "type": "type_blb"}, headers=HEADERS,
+                                data=batch_str)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Failed to send batch: {e}")
+        backup_container.write_event_to_blob(batch_data, e)
 
 
-def log_sender():
+def batch_creator(azeventhub):
+    local_log_batch = []
+    last_batch_time = time.time()
+    for event in azeventhub:
+        logs = process_eventhub_message(event)
+        for log in logs:
+            formatted_log = json.dumps(log) + '\n'
+            local_log_batch.append(formatted_log)
+
+        # Check the batch size or time interval after processing all logs from the event
+        current_time = time.time()
+        if len(local_log_batch) >= BUFFER_SIZE or (current_time - last_batch_time) >= INTERVAL_TIME:
+            print(f"Adding batch of size {len(local_log_batch)} to the sending queue.")
+            batch_queue.put(list(local_log_batch))  # Put a copy of the batch
+            local_log_batch.clear()  # Clear the local batch
+            last_batch_time = current_time
+
+    # Check and send any remaining logs in the batch after processing all events
+    if local_log_batch:
+        print(f"Adding batch of size {len(local_log_batch)} to the sending queue.")
+        batch_queue.put(list(local_log_batch))  # Put a copy of the batch
+        local_log_batch.clear()  # Clear the local batch
+
+
+
+def batch_sender():
     while True:
-        log = log_queue.get()
         try:
-            send_log(log)
-        except Exception as e:
-            logging.error(f"Failed to send log to Logz.io after retries: {e}")
-            backup_container.write_event_to_blob(log, e)
-        finally:
-            log_queue.task_done()
+            batch = batch_queue.get(timeout=0.1)
+            if batch:
+                send_batch(batch)
+                batch_queue.task_done()
+        except Empty:
+            continue
 
 
-def start_log_senders(thread_count=4):
+def start_batch_senders(thread_count=4):
     for _ in range(thread_count):
-        Thread(target=log_sender, daemon=True).start()
+        Thread(target=batch_sender, daemon=True).start()
 
 
 def process_eventhub_message(event):
     try:
         message_body = event.get_body().decode('utf-8')
-        return [json.loads(line) for line in message_body.splitlines()]
+        logs = []
+        for line in message_body.splitlines():
+            log_entry = json.loads(line)
+            # Check if this log entry contains nested logs under 'records'
+            if 'records' in log_entry and isinstance(log_entry['records'], list):
+                logs.extend(log_entry['records'])  # Add nested logs individually
+            else:
+                logs.append(log_entry)
+        return logs
     except Exception as e:
         logging.error(f"Error processing EventHub message: {e}")
         return []
 
 
 def main(azeventhub: List[func.EventHubEvent]):
-    for event in azeventhub:
-        logs = process_eventhub_message(event)
-        for log in logs:
-            log_queue.put(log)
-    logging.info('EventHub trigger processing complete')
-
-
-# Start log sender threads
-start_log_senders(thread_count=thread_count)  # Use the thread count from the environment variable
+    batch_creator_thread = Thread(target=batch_creator, args=(azeventhub,), daemon=True)
+    batch_creator_thread.start()
+    start_batch_senders(thread_count=thread_count)
+    batch_creator_thread.join()
+    logging.info('EventHub trigger processing complete!')
